4aaee5aca24980789f18c97cc30b858f0706b6b8
Nicholas McCollum <nimccoll@microsoft.com>
2019-08-02 13:42:52 -0400

    Add PowerShell script examples

---
+++ GetKeyVaultSecret.ps1
@@ -0,0 +1,26 @@
+﻿param (
+    [Parameter(Mandatory=$True)]
+    [string]
+    $servicePrincipal,
+    [Parameter(Mandatory=$True)]
+    [string]
+    $spPassword,
+    [Parameter(Mandatory=$True)]
+    $tenantId,
+    [Parameter(Mandatory=$True)]
+    $subscriptionId,
+    [Parameter(Mandatory=$True)]
+    [string]
+    $keyVaultName,
+    [Parameter(Mandatory=$True)]
+    [string]
+    $secretName
+)
+
+$passwd = ConvertTo-SecureString $spPassword -AsPlainText -Force
+$pscredential = New-Object System.Management.Automation.PSCredential($servicePrincipal, $passwd)
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -TenantId $tenantId
+
+Set-AzContext -Subscription $subscriptionId
+$secret = Get-AzKeyVaultSecret -vaultName $keyVaultName -name $secretName
+Write-Host "Value of secret " $secretName " is " $secret.SecretValueText
